e4032bddf481eea9187ec6206d624b3cc5a81f0e
Jeffrey Reed <JTReed1@gmail.com>
2013-06-04 10:32:41 -0400

    When does thin=s new version get pushed?

--- my-test-repo/Class1.cs
+++ my-test-repo/Class1.cs
@@ -9,5 +9,6 @@ namespace my_test_repo
     class Class1
     {
         // Hold this place for something magicial to happen.
+        // Here's more to test the pushing to the branch on the server
     }
 }
